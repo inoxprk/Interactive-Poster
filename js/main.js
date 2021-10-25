572315777e799057ddcd0616483376ef2c730b44
inoxprk <inho0819@gmail.com>
2021-10-25 18:03:53 -0400

    test1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new Swiper('.posters .swiper-container', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  // loop:true,
+  navigation: {
+    prevEl: '.posters .swiper-prev',
+    nextEl: '.posters .swiper-next'
+  }
+});
---
+++ poster1/p5.js
@@ -0,0 +1,51 @@
+let img //이미지 변수 선언
+function preload() { //이미지 변수에 이미지 디렉토리 할당
+  img = loadImage('./asset.png')
+}
+
+let scrollPosition = 0
+
+document.addEventListener('scroll', function(e) { //return current scroll position
+scrollPosition = Math.round(window.scrollY)
+// console.log(scrollPosition)
+})
+
+let bodyHeight = document.querySelector('*').scrollHeight *2 //return body scroll Height
+// console.log(bodyHeight)
+
+function setup(){
+  createCanvas(windowWidth, windowHeight*3) //html 사이즈만큼 캔버스 생성
+}
+
+function draw(){
+  let imgWidth
+  let yCoord
+
+  // const imgXcrd = (windowWidth-(windowWidth*(imgWidth/550.8)))*scrollPosition/bodyHeight
+  // const imgPctToWd = windowWidth*(imgWidth/550.8)
+
+  // function imgXcrd(imgWidthPx) {
+  //   return (windowWidth-(windowWidth*(imgWidthPx/550.8)))*scrollPosition/bodyHeight
+  // }
+  
+  function imgXcrd(imgWidthPx) {
+      return (windowWidth-(windowWidth*(imgWidthPx/632)))*scrollPosition/bodyHeight
+    }
+
+  function imgWdHt (imgWidthPx) {
+    return windowWidth*imgWidthPx/550.8
+  }
+
+  function imgYcrd(yCrdInPx) {
+    return scrollPosition+(windowHeight*(yCrdInPx/730.8))
+  }
+
+  background(25,255,255) 
+  imgWidth = 100
+  yCoord = 0
+  image(img,imgXcrd(imgWidth),imgYcrd(yCoord),imgWidth, imgWidth)
+
+  imgWidth = 200
+  yCoord = 300
+  image(img,imgXcrd(imgWidth),imgYcrd(yCoord),imgWidth, imgWidth)
+}
