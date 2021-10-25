951df14963863e2d5b8f8c96ca7d85ed9f3ee442
inoxprk <inho0819@gmail.com>
2021-10-25 18:24:11 -0400

    3rd fear

--- poster1/p5.js
+++ poster1/p5.js
@@ -1,46 +1,16 @@
 let img //이미지 변수 선언
+
 function preload() { //이미지 변수에 이미지 디렉토리 할당
   img = loadImage('./asset.png')
 }
 
-let scrollPosition = 0
-
-document.addEventListener('scroll', function(e) { //return current scroll position
-scrollPosition = Math.round(window.scrollY)
-// console.log(scrollPosition)
-})
-
-let bodyHeight = document.querySelector('*').scrollHeight *2 //return body scroll Height
-// console.log(bodyHeight)
-
 function setup(){
-  createCanvas(windowWidth, windowHeight*3) //html 사이즈만큼 캔버스 생성
+  createCanvas(windowWidth, windowHeight*3)
 }
 
 function draw(){
-  let imgWidth
-  let yCoord
 
-  // const imgXcrd = (windowWidth-(windowWidth*(imgWidth/550.8)))*scrollPosition/bodyHeight
-  // const imgPctToWd = windowWidth*(imgWidth/550.8)
-
-  // function imgXcrd(imgWidthPx) {
-  //   return (windowWidth-(windowWidth*(imgWidthPx/550.8)))*scrollPosition/bodyHeight
-  // }
-  
-  function imgXcrd(imgWidthPx) {
-      return (windowWidth-(windowWidth*(imgWidthPx/550.8)))*scrollPosition/bodyHeight
-    }
-
-  function imgWdHt (imgWidthPx) {
-    return windowWidth*imgWidthPx/550.8
-  }
-
-  function imgYcrd(yCrdInPx) {
-    return scrollPosition+(windowHeight*(yCrdInPx/730.8))
-  }
-
-  background(25,255,255) 
+  background(255,255,255) 
   imgWidth = 100
   yCoord = 0
   image(img,imgXcrd(imgWidth),imgYcrd(yCoord),imgWdHt(imgWidth), imgWdHt(imgWidth))
@@ -48,4 +18,25 @@ function draw(){
   imgWidth = 200
   yCoord = 300
   image(img,imgXcrd(imgWidth),imgYcrd(yCoord),imgWdHt(imgWidth), imgWdHt(imgWidth))
+}
+
+
+//이 아래는 사전적으로 정의된 함수들 ----------------------------------------------------------------------------------------------------
+let scrollPosition = 0
+document.addEventListener('scroll', function(e) { //return current scroll position
+scrollPosition = Math.round(window.scrollY)
+})
+let bodyHeight = document.querySelector('*').scrollHeight *2 //return body scroll Height
+
+let imgWidth
+let yCoord
+
+function imgXcrd(imgWidthPx) {
+    return (windowWidth-(windowWidth*(imgWidthPx/550.8)))*scrollPosition/bodyHeight
+  }
+function imgWdHt (imgWidthPx) {
+  return windowWidth*imgWidthPx/550.8
+}
+function imgYcrd(yCrdInPx) {
+  return scrollPosition+(windowHeight*(yCrdInPx/730.8))
 }
